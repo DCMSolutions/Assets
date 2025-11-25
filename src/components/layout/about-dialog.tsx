0e4a1ed8a71cba8643b7ebbb0d5b5015796dec78
Petiso13 <ian13celestino@gmail.com>
2025-11-25 11:50:42 -0300

    about dialog update

--- src/components/layout/about-dialog.tsx
+++ src/components/layout/about-dialog.tsx
@@ -14,8 +14,8 @@ export default function AboutDialog() {
           <DialogTitle>Assets</DialogTitle>
         </DialogHeader>
 
-        <p>Versión 0.7</p>
-        <p>Fecha: 20 de noviembre de 2025</p>
+        <p>Versión 0.8</p>
+        <p>Fecha: 25 de noviembre de 2025</p>
       </DialogContent>
     </Dialog>
   );
